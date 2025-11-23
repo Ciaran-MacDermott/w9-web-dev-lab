9d2d298ad77c3607a50b923844c8b9f4f8a32a7d
Ciaran-MacDermott <115147231+Ciaran-MacDermott@users.noreply.github.com>
2025-11-23 21:23:53 +0000

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,9 +1,8 @@
 function Compose() {
-  let output = "";
 
   for (let i = 99; i >= 1; i--) {
 
-  document.getElementById("mainBottle").innerHTML += `
+    document.getElementById("mainBottle").innerHTML += `
       ${i} bottles of beer on the wall, ${i} bottles of beer.<br/>
       Take one down and pass it around, ${i - 1} bottles of beer on the wall.<br/><br/>
     `;
