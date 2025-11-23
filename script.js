7f13ec8eca0f9efed8ddb50d14940f50837504b0
Ciaran-MacDermott <115147231+Ciaran-MacDermott@users.noreply.github.com>
2025-11-23 20:32:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,26 +6,24 @@ document.getElementById("play").onclick = function() {
 	//initialiser
   let result = "";
   let round = 0;
-  let compScore = 0;
+  let userScore = 0;
 
-  while (result <=3){
+  while (round <=3 && userScore <1){
   	//merged game-winning logic
-      if (userRound === compChoice) {
-          result = "Tie, refresh the page to play again";
-      } else if (
-          (userRound === "rock" && compChoice === "scissors") ||
-          (userRound === "paper" && compChoice === "rock") ||
-          (userRound === "scissors" && compChoice === "paper")
-      ) {
-          result = "You win";
-          round += 1;
-      } else {
-          result =  "You lose sorry";    
-          round += 1;
-      }
-  
-  
-      alert(result);
+	      if (userRound === compChoice) {
+	          result = "Tie, refresh the page to play again";
+	      } else if (
+	          (userRound === "rock" && compChoice === "scissors") ||
+	          (userRound === "paper" && compChoice === "rock") ||
+	          (userRound === "scissors" && compChoice === "paper")
+	      ) {
+	          result = "You win click to play again";
+	          round += 1;
+	      } else {
+	          result =  "You lose sorry" cick to play again;    
+	          round += 1;
+	      }
+		  alert(result);
   }
   alert("3 rounds are inished");
 };
